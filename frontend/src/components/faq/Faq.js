e7a29741455c24bf3eecb61fc4057653e92cce2a
Cole Anderson <cole@coles-mbp.lan>
2020-10-03 21:28:34 -0400

    FAQ template page ready

--- frontend/src/components/faq/Faq.js
+++ frontend/src/components/faq/Faq.js
@@ -6,6 +6,22 @@ function Faq() {
   return (
     <div className="faq">
       <h1>FAQ</h1>
+      <br></br><br></br>
+
+      <h2>Questions go Here</h2>
+      <p>Answers go here</p>
+      <a href="https://www.google.com" target="_blank">Google</a>
+      <br></br>
+
+      <h2>Questions go Here</h2>
+      <p>Answers go here</p>
+      <a href="https://www.google.com" target="_blank">Google</a>
+      <br></br>
+
+
+
+
+
     </div>
   );
 }
