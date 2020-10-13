271df3dfdf4d81a5b1709dd2cc38ab64413e0600
Michael Koohang <michael.s.k1001@gmail.com>
2020-10-13 15:30:19 -0400

    Add wait time algorithm

--- app/res/utils.js
+++ app/res/utils.js
@@ -1,19 +1,49 @@
 
+const db = require('../db');
 const {PythonShell} = require('python-shell');
+const queries = require('../res/queries.json');
 
 module.exports = {
   parseTime: (input) => {
-    let options = { args: [input] }
-    PythonShell.run('./res/timeParser.py', options, function (err, results) {
-      if (results[0] === "None") {
-        return null;
-      } else {
-        return parseInt(results[0]);
-      }
+    return new Promise((success, error) => {
+        let options = { args: [input] }
+        PythonShell.run('./res/timeParser.py', options, function (err, results) {
+          if (results[0] === "None") {
+            return success(null);
+          } else {
+            return success(parseInt(results[0]));
+          }
+        });
     });
   },
-  calcWaitTime: (input) => {
+  calcWaitTime: (minutes, locationId) => {
+    return db.query(queries.select_calculated_time + locationId + " )")
+      .then((result) => {
+        let runningAverage = 0;
+        let weight;
+        if (result.length > 0) {
+          runningAverage = result[0].estimated_time
+        }
 
+        if (minutes < 5.0) {
+          weight = 1.0;
+        } else if (minutes < 10.0) {
+          weight = 0.75;
+        } else if (minutes < 20.0) {
+          weight = 0.5;
+        } else if (minutes < 30.0) {
+          weight = 0.3;
+        } else if (minutes < 40.0) {
+          weight = 0.2;
+        } else if (minutes < 50.0) {
+          weight = 0.1;
+        } else {
+          weight = 0.05;
+        }
+
+        runningAverage = (1 - weight) * runningAverage + weight * minutes;
+        return db.query(queries.insert_calculated_time, [new Date(), runningAverage, locationId]);
+      });
   }
 }
 
--- app/routes/sms.js
+++ app/routes/sms.js
@@ -1,12 +1,12 @@
 
+const crypto = require('crypto');
 const db = require('../db');
 const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const queries = require('../res/queries.json');
 const router = express.Router();
-const winston = require('../res/winston');
 const utils = require('../res/utils');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const crypto = require('crypto');
+const winston = require('../res/winston');
 
 router.post('/sms', function(req, res) {
   const twiml = new MessagingResponse();
@@ -16,16 +16,12 @@ router.post('/sms', function(req, res) {
   const hashNum = crypto.createHmac('sha256', process.env.SECRET)
     .update(fromNum)
     .digest('hex');
-
   db.query(queries.select_phone_id + `'${hashNum}'`)
     .then((result) => {
       if (result.length > 0 && result[0].whitelisted === 1) {
         return result[0].id
       } else if (result.length > 0) {
-        twiml.message("We've already received your wait time report. Thanks for your participation!");
-        res.writeHead(200, {'Content-Type': 'text/xml'});
-        res.end(twiml.toString());
-        throw new Error("Message already received from this number.");
+        return -1
       } else {
         return db.query(queries.insert_phone, [hashNum])
           .then((result) => {
@@ -34,30 +30,43 @@ router.post('/sms', function(req, res) {
       }
     })
     .then(phoneId => {
-      return db.query(queries.select_location_id + toNum)
-      .then(function(result) {
-        const locationId = result[0].id;
-        return {"phoneId" : phoneId, "locationId" : locationId}
-      });
-    })
-    .then(result => {
-      const minutes = utils.parseTime(input);
-      if (!minutes) {
-        twiml.message("Thank you for sharing. This will help alert your neighbors and others to the wait time at this location. Please visit www.ourwebsite.com for more up to date information about wait times.");
-        res.writeHead(200, {'Content-Type': 'text/xml'});
-        res.end(twiml.toString());
-        const values = [new Date(), input, minutes, result.locationId, result.phoneId];
-        return db.query(queries.insert_accept, values);
+      if (phoneId === -1) {
+        return {"phoneId" : -1, "locationId" : null}
       } else {
-        twiml.message("Thank you for sharing. Unfortunately, we couldn't understand what you sent. Please send your wait time in following format: \"15 min\"");
-        res.writeHead(200, {'Content-Type': 'text/xml'});
-        res.end(twiml.toString());
-        const values = [new Date(), input, result.locationId, result.phoneId];
-        return db.query(queries.insert_reject, values);
+        return db.query(queries.select_location_id + toNum)
+          .then(function(result) {
+            const locationId = result[0].id;
+            return {"phoneId" : phoneId, "locationId" : locationId}
+          });
       }
     })
+    .then(result => {
+      return utils.parseTime(input)
+        .then(minutes => {
+          if (result.phoneId === -1) {
+            twiml.message("We've already received your wait time report. Thanks for your participation!");
+            res.writeHead(200, {'Content-Type': 'text/xml'});
+            res.end(twiml.toString());
+          } else if (!minutes) {
+            twiml.message("Thank you for sharing. Unfortunately, we couldn't understand what you sent. Please send your wait time in following format: \"15 min\"");
+            res.writeHead(200, {'Content-Type': 'text/xml'});
+            res.end(twiml.toString());
+            const values = [new Date(), input, result.locationId, result.phoneId];
+            return db.query(queries.insert_reject, values);
+          } else {
+            utils.calcWaitTime(minutes, result.locationId)
+              .then(() => {
+                twiml.message("Thank you for sharing. This will help alert your neighbors and others to the wait time at this location. Please visit www.ourwebsite.com for more up to date information about wait times.");
+                res.writeHead(200, {'Content-Type': 'text/xml'});
+                res.end(twiml.toString());
+                const values = [new Date(), input, minutes, result.locationId, result.phoneId];
+                return db.query(queries.insert_accept, values);
+              });
+
+          }
+        });
+    })
     .catch(function(err) {
-      res.status(500).json([{"error": err}])
       winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
       throw err
     });
