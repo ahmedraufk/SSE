1eb824cc5761a5a8d1cc7865b72f698cc3683247
Ahmed Rauf <ahmedraufk@gmail.com>
2020-10-27 18:19:06 -0400

    Remove commented code and faq json file

--- frontend/src/components/faq/Faq.js
+++ frontend/src/components/faq/Faq.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Accordion, Card, Button, Container} from 'react-bootstrap';
 import Menu from "../menu/Menu";
-import faq from '../../res/faq.json';
 import './Faq.css';
 
 function Faq() {
@@ -125,27 +124,6 @@ function Faq() {
                     </Card.Body>
                 </Accordion.Collapse>
             </Card>
-
-    {/*      { faq.map((question, i) => (
-              <Card key={i+1}>
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey={i+1}>
-                    {question.text}
-                  </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey={i+1}>
-                  <Card.Body>
-                      <p>{question.answer}</p>
-                      { question.img.length > 0 &&
-                        <img class="img-fluid" src = { require("../../res/img/" + question.img)}/>
-                      }
-
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-
-            ))
-          }*/}
         </Accordion>
       </Container>
     </div>
